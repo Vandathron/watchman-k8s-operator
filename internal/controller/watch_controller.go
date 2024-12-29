37ad5b1a9c84a82fcdf51bc711f95a2ae11fd7f4
Tosin <akinnagbelast@gmail.com>
2024-12-29 16:18:31 +0100

    internal watchman reconcilliation

--- internal/controller/watch_controller.go
+++ internal/controller/watch_controller.go
@@ -1,3 +1,46 @@
+func (r *WatchReconciler) reconcileWatchManResource(ctx context.Context, watch *auditv1alpha1.Watch) error {
+	log := log.FromContext(ctx)
+
+	selectors := watch.Spec.Selectors
+
+	for _, selector := range selectors {
+		ns := selector.Namespace
+		for _, kind := range selector.Kinds {
+			var objectList client.ObjectList
+			err := r.List(ctx, objectList, client.InNamespace(ns), client.MatchingFields{".kind": kind})
+			if err != nil {
+				log.Error(err, "Failed to fetch resources for kind: ", kind)
+				continue
+			}
+
+			objectKind := objectList.GetObjectKind().GroupVersionKind().Kind
+			if kind != objectKind {
+				log.Error(fmt.Errorf("resource kind mismatch"), "Resource internal kind:", kind, "does not match with retrieved kind", objectKind)
+				continue
+			}
+
+			if objectKind == "Deployment" {
+				deployments, ok := objectList.(*appsv1.DeploymentList)
+
+				if !ok {
+					log.Error(fmt.Errorf("parsing failed"), "Failed to parse object list to deployment struct")
+					continue
+				}
+
+				if err := r.watchDeployments(ctx, deployments); err != nil {
+					log.Error(err, "Failed to watch deployment")
+					continue
+				}
+
+				log.Info("Watching deployment resources in namespace", ns)
+			} else {
+				log.Error(fmt.Errorf("unsupported resource kind to watch"), "Invalid resource kind ", objectKind)
+			}
+		}
+	}
+
+	return nil
+}
 func (r *WatchReconciler) watchDeployments(ctx context.Context, deployments *appsv1.DeploymentList) error {
 	log := log.FromContext(ctx)
 
