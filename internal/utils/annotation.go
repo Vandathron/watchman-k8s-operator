e453adf606099b4888513845ff750fc4b23ab762
Tosin <akinnagbelast@gmail.com>
2024-12-31 18:41:03 +0100

    annotation utils

---
+++ internal/utils/annotation.go
@@ -0,0 +1,15 @@
+package utils
+
+const (
+	WatchByAnnotationKey = "audit.my.domain/watch-by"
+	WatchByAnnotationKV  = "watchman"
+
+	WatchActionTypeAnnotationKey = "audit.my.domain/watch-action"
+	WatchActionTypeCreate        = "Create"
+	WatchActionTypeDelete        = "Delete"
+	WatchActionTypeUpdate        = "Update"
+
+	WatchActorKey = "audit.my.domain/watch-actor"
+
+	WatchManFieldManager = "watch-man-manager"
+)
