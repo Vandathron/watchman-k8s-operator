acf9fee2fb7d5a364d7bc60070846ee929e1b783
Tosin <akinnagbelast@gmail.com>
2025-01-14 16:26:34 +0100

    helper to check supported kinds

--- internal/utils/resource.go
+++ internal/utils/resource.go
@@ -4,3 +4,14 @@ const (
 	SupportedKindService    string = "Service"
 	SupportedKindDeployment string = "Deployment"
 )
+
+var supports = map[string]bool{SupportedKindService: true, SupportedKindDeployment: true}
+
+func SupportsAllKinds(kinds ...string) bool {
+	for _, kind := range kinds {
+		if _, ok := supports[kind]; !ok {
+			return false
+		}
+	}
+	return true
+}
