61d345ed874b7d696abc7dccf52a55f985fcf363
Tosin <akinnagbelast@gmail.com>
2025-01-14 14:50:02 +0100

    Test when watch resource spec is updated

--- internal/controller/watch_controller_test.go
+++ internal/controller/watch_controller_test.go
@@ -129,6 +129,105 @@ var _ = Describe("Watch Controller", func() {
 
 		})
 
+		When("watch resource spec selector is updated", func() {
+			deployResourceCnt := 2
+			svcResourceCnt := 2
+			var once sync.Once
+
+			BeforeEach(func() {
+				By("Setting up deployments and services")
+				once.Do(func() {
+					testCreateDeployments(
+						makeDeploymentSpec("dep-3", ns1),
+						makeDeploymentSpec("dep-4", ns1),
+						makeDeploymentSpec("dep-5", ns2),
+						makeDeploymentSpec("dep-6", ns2))
+
+					testCreateServices(
+						makeSvcSpec("svc-3", ns1),
+						makeSvcSpec("svc-4", ns1),
+						makeSvcSpec("svc-5", ns2),
+						makeSvcSpec("svc-6", ns2))
+
+					watch.Spec.Selectors = []auditv1alpha1.WatchSelector{
+						{Namespace: ns1, Kinds: []string{"Service"}},               // remove service
+						{Namespace: ns2, Kinds: []string{"Service", "Deployment"}}, // add a new namespace to watch for kinds service and deployment
+					}
+					Expect(k8sClient.Update(ctx, watch)).To(Succeed())
+					Eventually(ctx, func(g Gomega) {
+						Expect(k8sClient.Get(ctx, typeNamespacedName, watch)).To(Succeed())
+						Expect(len(watch.Spec.Selectors[0].Kinds)).To(Equal(1))
+					}, timeout, interval).Should(Succeed())
+				})
+			})
+
+			It("should update all necessary resources as per updated watch spec config", func() {
+
+				r := &WatchReconciler{
+					Client: k8sClient,
+					Scheme: k8sClient.Scheme(),
+					Audit:  &audit.Console{},
+				}
+				cm := &v1.ConfigMap{}
+
+				_, err := r.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName}) // reconcile to update config map
+				Expect(err).To(BeNil())
+
+				By("Updating watch resource config map")
+				Eventually(ctx, func(g Gomega) {
+					g.Expect(k8sClient.Get(ctx, typeNamespacedName, cm)).To(Succeed())
+					watchedKinds := utils.ExtractWatchedKindsFromCM(cm.Data)
+					g.Expect(len(watchedKinds)).To(Equal(2)) // test that cm has both namespaces (ns1,ns2)
+
+					// Test that cm contains ns1 and watches only a kind which is Service
+					g.Expect(len(watchedKinds[ns1])).To(Equal(1))
+					g.Expect(slices.Index(watchedKinds[ns1], "Service")).ShouldNot(BeEquivalentTo(-1))
+
+					// Test that cm contains second namespace and watches two kinds which is Service and Deployment
+					g.Expect(len(watchedKinds[ns2])).To(Equal(2))
+					g.Expect(slices.Index(watchedKinds[ns2], "Service")).ShouldNot(BeEquivalentTo(-1))
+					g.Expect(slices.Index(watchedKinds[ns2], "Deployment")).ShouldNot(BeEquivalentTo(-1))
+				})
+
+				By("Removing watch annotation on resources no longer being watched")
+				Eventually(ctx, func(g Gomega) {
+					Expect(err).To(BeNil())
+					deployedList := &appsv1.DeploymentList{}
+					svcList := &v1.ServiceList{}
+
+					By("Ensuring deployments in ns1 are not watched")
+					Expect(k8sClient.List(ctx, deployedList, client.InNamespace(ns1))).To(Succeed())
+					Expect(deployedList.Items).NotTo(BeNil())
+					Expect(len(deployedList.Items)).Should(BeNumerically(">=", deployResourceCnt))
+					for _, deployment := range deployedList.Items {
+						Expect(utils.HasWatchManAnnotation(deployment.Annotations, utils.WatchByAnnotationKey, utils.WatchByAnnotationKV)).To(BeFalse())
+					}
+
+					By("Ensuring services in ns1 are watched")
+					Expect(k8sClient.List(ctx, svcList, client.InNamespace(ns1))).To(Succeed())
+					Expect(svcList.Items).NotTo(BeNil())
+					Expect(len(svcList.Items)).Should(BeNumerically(">=", svcResourceCnt))
+					for _, svc := range svcList.Items {
+						Expect(utils.HasWatchManAnnotation(svc.Annotations, utils.WatchByAnnotationKey, utils.WatchByAnnotationKV)).To(BeTrue())
+					}
+
+					By("Ensuring both services and deployments in ns2 are watched")
+					deployedList = &appsv1.DeploymentList{}
+					svcList = &v1.ServiceList{}
+					Expect(k8sClient.List(ctx, deployedList, client.InNamespace(ns2))).To(Succeed())
+					Expect(deployedList.Items).NotTo(BeNil())
+					Expect(len(deployedList.Items)).Should(BeNumerically(">=", deployResourceCnt))
+					for _, deployment := range deployedList.Items {
+						Expect(utils.HasWatchManAnnotation(deployment.Annotations, utils.WatchByAnnotationKey, utils.WatchByAnnotationKV)).To(BeTrue())
+					}
+					Expect(k8sClient.List(ctx, svcList, client.InNamespace(ns2))).To(Succeed())
+					Expect(svcList.Items).NotTo(BeNil())
+					for _, svc := range svcList.Items {
+						Expect(utils.HasWatchManAnnotation(svc.Annotations, utils.WatchByAnnotationKey, utils.WatchByAnnotationKV)).To(BeTrue())
+					}
+				}, timeout, interval).Should(Succeed())
+			})
+		})
 	})
 })
 
